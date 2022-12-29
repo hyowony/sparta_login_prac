81aa4870af6791e746823344a73dca2ea6fa812e
hyowony <118158856+hyowony@users.noreply.github.com>
2022-12-29 20:32:24 +0900

    api 만들기 완료

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ app.get('/', (req,res)=> {
   res.send('첫 프로젝트!')
 })
 
+// app.listen(port, ()=> {
+//   console.log('서버가 열렸어요!')
+// )}
+
+app.get('/user', (req,res)=> {
+  res.send('회원정보 가져오기')
+})
+
+app.post('/login', (req,res)=> {
+  res.send('로그인 페이지')
+})
+
+app.post('/logout', (req,res)=> {
+  res.send('로그아웃 페이지')
+})
+app.post('/register',(req,res)=> {
+  res.send('등록하기')
+})
+
 app.listen(port, ()=> {
   console.log('서버가 열렸어요!')
 })
